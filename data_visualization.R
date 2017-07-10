bfa709f1a6ff5042063833c51df2b5eb1bc08530
emu <himtiaz@dohatec.com.bd>
2017-07-10 11:10:39 +0600

    Data Visualization complete

--- data_visualization.R
+++ data_visualization.R
@@ -1,29 +1,32 @@
 library(ggplot2)
 
+# Create a frequency bar chart
 ggplot(data = mtcars, aes(x = am)) + 
        geom_bar() + 
-  ggtitle("Car count based on Transmission Type [0 = manual, 1 = auto]") +
+  ggtitle("Car count based on Transmission Type [0 = auto, 1 = manual]") +
   xlab("Transmission Type") +
   ylab("Total cars")
       
-       
+# Create a histogram       
 ggplot(data = mtcars, aes(x = qsec)) + 
   geom_histogram(bins = 10) + 
   ggtitle("Distribution of Fuel") +
   xlab("Fuel per Milage") +
-  ylab("Total cars")
+  ylab("Count of cars")
 
+# Create a density plot
 ggplot(data = mtcars, aes(x = qsec)) + 
   geom_density() + 
-  ggtitle("Distribution of Fuel") +
+  ggtitle("Distribution of Fuel Economy") +
   xlab("Fuel per Milage") +
-  ylab("Total cars")
+  ylab("Density")
 
+# Create a scatterplot
 ggplot(data = mtcars, aes(x = cyl, y = qsec)) + 
   geom_point() + 
-  ggtitle("Distribution of Fuel") +
-  xlab("cylinders") +
-  ylab("Total milage")
+  ggtitle("Fuel Economy by Cylinders") +
+  xlab("Number of Cylinders") +
+  ylab("Fuel Economy (mpg)")
 
 
 
